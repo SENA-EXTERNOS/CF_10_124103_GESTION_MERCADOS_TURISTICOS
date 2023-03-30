51b3232a6bb9239a6a301a4893cdf10b998578e4
deividramirezarias <deivid.innovatec@gmail.com>
2023-03-29 21:17:46 -0500

    ajustes
    ajustes

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -193,7 +193,7 @@
     .row.my-5(data-aos="fade-zoom")
       figure
         .video
-          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/-OZhkkU2FYY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     separador
     h2(data-aos="fade-up-right") Técnicas y herramientas
